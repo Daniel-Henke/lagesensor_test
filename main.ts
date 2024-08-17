99be9f70fd01dcfb3a149d41e7b7a872eeeed941
Daniel-Henke <d-henke@web.de>
2024-08-17 15:49:15 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+basic.showString("Lagesensor_Test")
+loops.everyInterval(500, function () {
+    serial.writeValue("Drehung X ", input.rotation(Rotation.Pitch))
+    serial.writeValue("Drehung Y ", input.rotation(Rotation.Roll))
+    serial.writeValue("Kompass ", input.compassHeading())
+    serial.writeValue("Beschleunigung X ", input.acceleration(Dimension.X))
+    serial.writeValue("Beschleunigung Y ", input.acceleration(Dimension.Y))
+    serial.writeValue("Beschleunigung Z ", input.acceleration(Dimension.Z))
+    serial.writeValue("Beschleunigung abs ", input.acceleration(Dimension.Strength))
+})
 basic.forever(function () {
 	
 })
